7f3a3ef18211a60715a1d808e04f3072357254ef
moon777 <myj2139@gmail.com>
2024-11-29 18:29:29 +0900

    [문영주] - sns 프로젝트 환경설정

---
+++ src/main/java/com/bbar/sns/HelloController.java
@@ -0,0 +1,16 @@
+package com.bbar.sns;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@GetMapping("/hello")
+	public String getHello() {
+		return "Hello World!!!!!";
+	}
+
+}
---
+++ src/test/java/com/bbar/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbar.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
