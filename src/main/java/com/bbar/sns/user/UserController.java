0bf25cea77ee70266828e89508103a2dba7a4721
moon777 <myj2139@gmail.com>
2024-12-02 16:02:11 +0900

    [문영주] - sns 프로젝트

---
+++ src/main/java/com/bbar/sns/user/UserController.java
@@ -0,0 +1,21 @@
+package com.bbar.sns.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	@GetMapping("/login-view")
+	public String inputLogin() {
+		return "user/login";
+	}
+	
+	@GetMapping("/join-view")
+	public String inputJoin() {
+		return "user/join";
+	}
+
+}
